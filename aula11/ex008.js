0535ee4af53b5446d3bdbe3c3d3af6c4af152a52
NicolasMenezes1 <135354623+NicolasMenezes1@users.noreply.github.com>
2023-09-18 12:28:24 -0300

    atualizar

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 60
+
+console.log(`O carro estava á ${vel}km/h`)
+
+if (vel>60) {
+    console.log('Ultrapassou o limite. MULTADO!')
+} else {
+    console.log('Dirija sempre com o cinto de segurança')
+}
+
+
