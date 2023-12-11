12495cba12de23cb2ddb33a06f57091ddd0e76ec
NicolasMenezes1 <135354623+NicolasMenezes1@users.noreply.github.com>
2023-12-11 13:08:26 -0300

    n

---
+++ Battisti/Aula 14 - Operadores Condicionais_Else e Elseif/script.js
@@ -0,0 +1,10 @@
+var idade = 18;
+
+
+if(idade >= 18 & idade < 60) {
+    console.log('Voto obrigatório');
+} else if(idade >= 16 & idade <= 18) {
+    console.log('Voto não é obrigatório');
+} else{
+    console.log('não é permitido votar');
+}
---
+++ Battisti/Aula 15 - Operadores de Comparação_Tipo e Valor/script.js
@@ -0,0 +1,10 @@
+var numero = 5
+
+if(numero == 5){
+    console.log('O número é 5, vendo apenas valor')
+}
+
+
+if(numero === 5){
+    console.log('O número é 5, vendo apenas valor e tipo. Identico')
+}
